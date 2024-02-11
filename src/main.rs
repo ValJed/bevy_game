b946502439fb671001c79bd5b1a2e60b239de9e2
Jed <vjeudy@protonmail.com>
2024-02-11 22:45:51 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
-use bevy::prelude::*;
-
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
 pub struct MainPlugin;
 
 impl Plugin for MainPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, add_people)
-            .add_systems(Update, (hello_world, greet_people));
+        app.add_systems(Startup, setup)
+            .add_systems(Update, move_character);
     }
 }
 
@@ -19,18 +13,44 @@ fn main() {
     App::new().add_plugins((DefaultPlugins, MainPlugin)).run();
 }
 
-fn hello_world() {
-    println!("hello world!");
-}
+fn move_character(
+    mut characters: Query<(&mut Transform, &Sprite)>,
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    println!("characters: {:?}", characters);
+    for (mut transform, _) in &mut characters {
+        println!("transform: {:?}", transform);
+        if input.pressed(KeyCode::Z) {
+            transform.translation.y += 100.0 * time.delta_seconds()
+        }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+        if input.pressed(KeyCode::S) {
+            transform.translation.y -= 100.0 * time.delta_seconds()
+        }
+
+        if input.pressed(KeyCode::D) {
+            transform.translation.x += 100.0 * time.delta_seconds()
+        }
+
+        if input.pressed(KeyCode::Q) {
+            transform.translation.y -= 100.0 * time.delta_seconds()
+        }
     }
 }
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes
+            .add(shape::Quad::new(Vec2::new(30., 30.)).into())
+            .into(),
+        transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
+        material: materials.add(ColorMaterial::from(Color::GOLD)),
+        ..default()
+    });
 }
