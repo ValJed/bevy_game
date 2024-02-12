d5e108b43b3f7e5e1fc1e8ab756374fe079cd54f
Jed <vjeudy@protonmail.com>
2024-02-12 23:24:54 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::prelude::*;
 
 pub struct MainPlugin;
 
@@ -12,49 +12,82 @@ impl Plugin for MainPlugin {
 #[derive(Component)]
 struct Player;
 
+#[derive(Component)]
+struct AnimationIndices {
+    first: usize,
+    last: usize,
+}
+
 fn main() {
     App::new().add_plugins((DefaultPlugins, MainPlugin)).run();
 }
 
+fn window_collision(mut characters: Query<&mut Transform, &Player>) {}
+
 fn move_character(
-    mut characters: Query<&mut Transform, &Player>,
+    mut characters: Query<(
+        &mut Transform,
+        &mut TextureAtlasSprite,
+        &AnimationIndices,
+        &Player,
+    )>,
     input: Res<Input<KeyCode>>,
     time: Res<Time>,
 ) {
-    for mut transform in &mut characters {
+    for (mut transform, mut sprite, indices, _) in &mut characters {
         if input.pressed(KeyCode::Z) {
             transform.translation.y += 100.0 * time.delta_seconds();
         }
 
         if input.pressed(KeyCode::S) {
-            transform.translation.y -= 100.0 * time.delta_seconds()
+            transform.translation.y -= 100.0 * time.delta_seconds();
         }
 
         if input.pressed(KeyCode::D) {
-            transform.translation.x += 100.0 * time.delta_seconds()
+            transform.translation.x += 100.0 * time.delta_seconds();
+            if sprite.index == indices.last {
+                sprite.index = 0;
+            } else {
+                sprite.index += 1;
+            }
         }
 
         if input.pressed(KeyCode::Q) {
-            transform.translation.x -= 100.0 * time.delta_seconds()
+            transform.translation.x -= 100.0 * time.delta_seconds();
+            sprite.index = 1;
+        }
+
+        if !input.pressed(KeyCode::Z)
+            && !input.pressed(KeyCode::S)
+            && !input.pressed(KeyCode::D)
+            && !input.pressed(KeyCode::Q)
+        {
+            sprite.index = 0;
         }
     }
 }
 
 fn setup(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
     commands.spawn(Camera2dBundle::default());
+
+    let texture_handle = asset_server.load("characters/frog/run.png");
+    let texture_atlas =
+        TextureAtlas::from_grid(texture_handle, Vec2::new(32., 32.), 12, 1, None, None);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    let animation_indices = AnimationIndices { first: 1, last: 11 };
+
     commands.spawn((
-        MaterialMesh2dBundle {
-            mesh: meshes
-                .add(shape::Quad::new(Vec2::new(30., 30.)).into())
-                .into(),
-            transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
-            material: materials.add(ColorMaterial::from(Color::GOLD)),
+        SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            sprite: TextureAtlasSprite::new(0),
+            transform: Transform::from_scale(Vec3::splat(1.0)), // Set the scale of the sprite
             ..default()
         },
+        animation_indices,
         Player {},
     ));
 }
