247454b3bc76d227b80de968f2232a2f4abe9152
Jed <vjeudy@protonmail.com>
2024-03-09 18:44:59 +0100

    use rapier

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use bevy::prelude::*;
 use bevy::time::Timer;
+use bevy_rapier2d::prelude::*;
 
 pub struct MainPlugin;
 
 impl Plugin for MainPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup)
+            .add_plugins(DefaultPlugins)
+            .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
             .insert_resource(PlayerInput::new())
             .add_systems(Update, movement_input)
             .add_systems(Update, move_character)
@@ -53,9 +56,7 @@ fn main() {
     App::new().add_plugins((DefaultPlugins, MainPlugin)).run();
 }
 
-fn window_collision(mut characters: Query<&mut Transform, &Player>) {}
-
-fn movement_input(mut player_input: ResMut<PlayerInput>, input: Res<Input<KeyCode>>) {
+fn movement_input(mut player_input: ResMut<PlayerInput>, input: Res<ButtonInput<KeyCode>>) {
     player_input.right = input.pressed(KeyCode::D);
     player_input.left = input.pressed(KeyCode::Q);
     player_input.jump = input.pressed(KeyCode::Space);
@@ -63,7 +64,7 @@ fn movement_input(mut player_input: ResMut<PlayerInput>, input: Res<Input<KeyCod
 
 fn update_char_sprite(
     mut query: Query<(
-        &mut TextureAtlasSprite,
+        &mut TextureAtlas,
         &AnimationIndices,
         &mut SpriteTimer,
         With<Player>,
@@ -71,26 +72,26 @@ fn update_char_sprite(
     player_input: Res<PlayerInput>,
     time: Res<Time>,
 ) {
-    let Ok((mut sprite, indices, mut timer, _)) = query.get_single_mut() else {
+    let Ok((mut atlas, indices, mut timer, _)) = query.get_single_mut() else {
         return;
     };
 
     if player_input.right {
         if timer.0.tick(time.delta()).just_finished() {
-            if sprite.index == indices.last {
-                sprite.index = 0;
+            if atlas.index == indices.last {
+                atlas.index = 0;
             } else {
-                sprite.index += 1;
+                atlas.index += 1;
             }
         }
     }
 
     if player_input.left {
         if timer.0.tick(time.delta()).just_finished() {
-            if sprite.index == indices.last {
-                sprite.index = 0;
+            if atlas.index == indices.last {
+                atlas.index = 0;
             } else {
-                sprite.index += 1;
+                atlas.index += 1;
             }
         }
     }
@@ -123,11 +124,11 @@ fn player_jump(
 fn player_fall(mut query: Query<(Entity, &mut Transform, &mut Jump), With<Player>>) {}
 
 fn move_character(
-    mut characters: Query<(&mut Transform, &mut TextureAtlasSprite, &mut Jump, &Player)>,
+    mut characters: Query<(&mut Transform, &mut TextureAtlas, &mut Jump, &Player)>,
     player_input: Res<PlayerInput>,
     time: Res<Time>,
 ) {
-    for (mut transform, mut sprite, mut jump, _) in &mut characters {
+    for (mut transform, mut atlas, mut jump, _) in &mut characters {
         if player_input.right {
             transform.translation.x += 100.0 * time.delta_seconds();
             transform.rotation = Quat::default();
@@ -139,7 +140,7 @@ fn move_character(
         }
 
         if !player_input.left && !player_input.right {
-            sprite.index = 0;
+            atlas.index = 0;
         }
     }
 }
@@ -147,27 +148,57 @@ fn move_character(
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    mut texture_atlases: ResMut<Assets<TextureAtlasLayout>>,
 ) {
     commands.spawn(Camera2dBundle::default());
 
     let texture_handle = asset_server.load("characters/frog/run.png");
-    let texture_atlas =
-        TextureAtlas::from_grid(texture_handle, Vec2::new(32., 32.), 12, 1, None, None);
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    let layout = TextureAtlasLayout::from_grid(Vec2::new(32., 32.), 12, 1, None, None);
     let animation_indices = AnimationIndices { first: 1, last: 11 };
+    // let rigid_body = RigidBodyBundle {
+    //     mass_properties: RigidBodyMassPropsFlags::ROTATION_LOCKED.into(),
+    //     activation: RigidBodyActivation::cannot_sleep(),
+    //     ccd: RigidBodyCcd {
+    //         ccd_enabled: true,
+    //         ..Default::default()
+    //     },
+    //     ..Default::default()
+    // };
+    // let collider = ColliderBundle {
+    //     shape: ColliderShape::cuboid(0.5, 0.5),
+    //     flags: ColliderFlags {
+    //         active_events: ActiveEvents::CONTACT_EVENTS,
+    //         ..Default::default()
+    //     },
+    //     ..Default::default()
+    // };
+    /* Create the ground. */
+    commands.spawn((
+        Collider::cuboid(500.0, 50.0),
+        TransformBundle::from(Transform::from_xyz(0.0, -100.0, 0.0)),
+    ));
+
+    /* Create the bouncing ball. */
+    commands
+        .spawn(RigidBody::Dynamic)
+        .insert(Collider::ball(50.0))
+        .insert(Restitution::coefficient(0.7))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, 400.0, 0.0)));
 
     commands
         .spawn((
             SpriteSheetBundle {
-                texture_atlas: texture_atlas_handle,
-                sprite: TextureAtlasSprite::new(0),
+                atlas: TextureAtlas { layout, index: 0 },
+                texture: texture_handle,
+                sprite: Sprite::new(0),
                 transform: Transform::from_scale(Vec3::splat(1.0)), // Set the scale of the sprite
                 ..default()
             },
             animation_indices,
             Player {},
             Jump(0.0),
+            // rigid_body,
+            // collider,
         ))
         .insert(SpriteTimer(Timer::from_seconds(0.05, TimerMode::Repeating)));
 }
